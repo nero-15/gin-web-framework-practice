5caed565e409643d871ad1390c8dd1f57721585d
nero-15 <ti7805@gmail.com>
2021-03-12 22:22:33 +0900

    lets encript

---
+++ LetsEncript/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Ping handler
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	//log.Fatal(autotls.Run(r, "example1.com", "example2.com"))
+}
